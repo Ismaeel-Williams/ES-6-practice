bbc69c6b6d9a7ba58bb7a013c8dc028794107ad3
Ismaeel <Ismaeelwilliams26@gmail.com>
2024-03-06 15:02:24 +0200

    modules-3.

---
+++ main.js
@@ -0,0 +1,2 @@
+//main.js
+import { add } from "math.js"
---
+++ modules.js
@@ -0,0 +1,22 @@
+//Modules:
+//Modules in javascript are a way to organize  code into seperate files or modules.
+//They allow you to break down your code into smaller, manageable pieces and facilitate reuse accross different parts of your application.
+//ES6 introduced natice support for modules in JavaScript.
+
+//user  type="module"  in the scropt tag to instruct the web browser to load a JavaScript file as a module.
+
+//1. Module Definition:
+//A module in JavaScript is defined by creating a seperate file that contains its own code.
+//Each file is treated as a seperate module, and u can export specefic parts of the module to be used in other modules
+
+//2. Exporting from a Module
+//To export a functionality from a module, you use the export keyword followed by the 
+//name of the variable, function, or class you want to export.
+
+//math.js
+
+//3. Importing into a Module: 
+//To import functionality from a module, you use the import keyword followed by the name of the variable, function, or 
+//class you want to import to import and the path to the module file.
+
+//main.js
